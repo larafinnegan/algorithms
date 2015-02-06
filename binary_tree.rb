4862a73f14702cbcced13f766611006c7fe456e2
Lara Finnegan <lcfinnegan@gmail.com>
2015-02-05 19:34:00 -0600

    initial commit

---
+++ binary_tree.rb
@@ -0,0 +1,24 @@
+class Node
+  attr_accessor :value, :parent, :child
+
+  def initialize
+    @value = 100
+    @parent = parent
+    @child = child
+  end
+
+
+end
+
+p Node.new
+
+
+def build_tree(array)
+  
+
+end
+
+
+
+
+p build_tree([1,2,3,4,5,6,7,8,9])
