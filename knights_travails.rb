baa10670233ffc108ea2d1c4b311dd6ebe9b07d0
Lara Finnegan <lcfinnegan@gmail.com>
2015-02-09 22:45:05 -0600

    created a tree of possible moves

--- knights_travails.rb
+++ knights_travails.rb
@@ -13,65 +13,72 @@ class Board
 
   def initialize(board = Array.new(8, [0, 1, 2, 3, 4, 5, 6, 7]))
     @board = board
-    
   end
 
 
-  def possible_moves(node)
-    node[0] = down
-    node[1] = across
+  def possible_moves(value)
+    down = value[0] 
+    across = value[1]
     moves = [[down - 2, across + 1], [down - 2, across - 1],
              [down - 1, across + 2], [down + 1, across + 2],
              [down + 2, across + 1], [down + 2, across - 1],
              [down - 1, across - 2], [down + 1, across - 2]]
-    possible_moves = moves.select {|x| p x.all? {|y| y < board.length-1 && y > 0}}
+    possible_moves = moves.select {|x| x.all? {|y| y < board.length && y >= 0}}
   end
 
-  def create_nodes(coordinates=nil)
-    root = Node.new(coordinates)
-    board.each {|x| p x.index}
+  def create_root(coordinates = [3,3], destination = [4,3])
+    node = Node.new(coordinates)
+    place_nodes(node, destination)
+    node
   end
 
-  def place_nodes()
-
+  def place_nodes(node, destination)
+    queue = [node]
+    created_nodes = [node]
+    until created_nodes.size == board.flatten.size
+      child_nodes = possible_moves(queue[0].coordinates) 
+      child_nodes.each do |val| 
+        if created_nodes.none? {|x| val == x.coordinates}
+          node = Node.new(val, queue[0])
+          created_nodes << node
+          queue << node
+        end
+      end
+      p queue[0].coordinates
+      return queue[0].coordinates if queue[0].coordinates == destination
+      queue.shift
+    end
   end
-
 
 end
 
 
-class Knight
-  attr_accessor :position, :destination, :board
+  class Knight
+    attr_accessor :position, :destination, :board
 
-  def initialize(position = []. destination = [], board = Board.new)
-    @position = position
-    @destination = destination
-    @board = board
+    def initialize(position = []. destination = [], board = Board.new)
+      @position = position
+      @destination = destination
+      @board = board
+    end
+
+    def get_position
+      puts "Please enter the knight's current coordinates:\n\nDown (0-7):"
+      down = gets.chomp.to_i
+      puts "Across (0-7):"
+      across = gets.chomp.to_i
+      position << down << across
+    end
+
+    def get_destination
+      puts "Please enter the knight's destination coordinates:\n\nDown (0-7):"
+      down = gets.chomp.to_i
+      puts "Across (0-7):"
+      across = gets.chomp.to_i
+      destination << down << across
+    end
   end
 
-  def get_position
-    puts "Please enter the knight's current coordinates:\n\nDown (0-7):"
-    down = gets.chomp.to_i
-    puts "Across (0-7):"
-    across = gets.chomp.to_i
-    position << down << across
-  end
 
-  def get_destination
-    puts "Please enter the knight's destination coordinates:\n\nDown (0-7):"
-    down = gets.chomp.to_i
-    puts "Across (0-7):"
-    across = gets.chomp.to_i
-    destination << down << across
-  end
-
-
-end
-
-
-board = Array.new(8, [0, 1, 2, 3, 4, 5, 6, 7])
-
-
-
-
-
+  board = Board.new
+  board.create_root
